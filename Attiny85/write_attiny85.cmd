a004080d15b96608886f53473c8707597f8062b7
Daniel Tavares <dantavares@gmail.com>
2023-11-21 12:07:11 -0300

    Update write_attiny85.cmd

--- Attiny85/write_attiny85.cmd
+++ Attiny85/write_attiny85.cmd
@@ -1,13 +1,13 @@
 @echo off
 
------------------------
---Set Arduino COM Port
------------------------
+:::::::::::::::::::::::
+::Set Arduino COM Port
+:::::::::::::::::::::::
 SET COM=COM1
 
--------------------------------------------------------------------
--- Do not change the line below unless you know what you are doing!
--------------------------------------------------------------------
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Do not change the line below unless you know what you are doing!
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 ..\avrdude -Cattiny85.conf -v -pattiny85 -cstk500v1 -P%COM% -b19200 -D -Uhfuse:w:hex_files\hfuse.hex:i -Ulfuse:w:hex_files\lfuse.hex:i -Uefuse:w:hex_files\efuse.hex:i -Ulock:w:hex_files\lock.hex:i -Uflash:w:hex_files\flash.hex:i
 

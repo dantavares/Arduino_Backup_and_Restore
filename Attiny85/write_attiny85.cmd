f2c153464d0d678018fa5faf1b2d8b3e7ab3cfeb
Daniel Tavares <dantavares@gmail.com>
2023-11-21 11:14:23 -0300

    Update write_attiny85.cmd

--- Attiny85/write_attiny85.cmd
+++ Attiny85/write_attiny85.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-SET COM=COM3
+SET COM=COM1
 
 ..\avrdude -Cattiny85.conf -v -pattiny85 -cstk500v1 -P%COM% -b19200 -D -Uhfuse:w:hex_files\hfuse.hex:i -Ulfuse:w:hex_files\lfuse.hex:i -Uefuse:w:hex_files\efuse.hex:i -Ulock:w:hex_files\lock.hex:i -Uflash:w:hex_files\flash.hex:i
 

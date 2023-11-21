5c00fd097e97a624d2e436aec7c189f43886713c
Daniel Tavares <dantavares@gmail.com>
2023-11-21 11:04:11 -0300

    Update and rename write_atmega328p.cmd to Atmega 328p/write_atmega328p.cmd

--- Atmega 328p/write_atmega328p.cmd
+++ Atmega 328p/write_atmega328p.cmd
@@ -1,13 +1,13 @@
 @echo off
 
 ----Arduino Pro / Mini
-SET BAUD=57600
+--SET BAUD=57600
 
 ----Arduino UNO
---SET BAUD=115200
+SET BAUD=115200
 
 --Set Arduino COM Port
-SET COM=COM3
+SET COM=COM1
 
 ..\avrdude -C..\avrdude.conf -v -patmega328p -carduino -P%COM% -b%BAUD% -D -Uflash:w:flash.hex:i
 

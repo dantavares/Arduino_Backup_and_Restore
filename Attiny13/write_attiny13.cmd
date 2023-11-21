7b443a007e68acad970f047f28e2ce71e98e45c1
Daniel Tavares <dantavares@gmail.com>
2023-11-21 11:38:44 -0300

    Update write_attiny13.cmd

--- Attiny13/write_attiny13.cmd
+++ Attiny13/write_attiny13.cmd
@@ -1,7 +1,13 @@
 @echo off
 
+-----------------------
 --Set Arduino COM Port
+-----------------------
 SET COM=COM1
+
+-------------------------------------------------------------------
+-- Do not change the line below unless you know what you are doing!
+-------------------------------------------------------------------
 
 ..\avrdude -Cattiny13.conf -v -pattiny13 -cstk500v1 -P%COM% -b19200 -Uhfuse:w:hex_files\hfuse.hex:i -Ulfuse:w:hex_files\lfuse.hex:i -Uefuse:w:hex_files\efuse.hex:i -Ulock:w:hex_files\lock.hex:i -Uflash:w:hex_files\flash.hex:i
 

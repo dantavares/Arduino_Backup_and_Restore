ec2ed8c0607b8eb02a47af00e137c0f8b4c9210f
Daniel Tavares <dantavares@gmail.com>
2023-11-21 11:06:40 -0300

    Update and rename read_attiny13.cmd to Attiny13/read_attiny13.cmd

---
+++ Attiny13/read_attiny13.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+--Set Arduino COM Port
+SET COM=COM1
+
+..\avrdude -Cattiny13.conf -v -pattiny13 -cstk500v1 -P%COM% -b19200 -Uhfuse:r:hex_files\hfuse.hex:i -Ulfuse:r:hex_files\lfuse.hex:i -Uefuse:r:hex_files\efuse.hex:i -Ulock:r:hex_files\lock.hex:i -Uflash:r:hex_files\flash.hex:i
+
+pause
--- read_attiny13.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-..\avrdude -Cattiny13.conf -v -pattiny13 -cstk500v1 -PCOM3 -b19200 -Uhfuse:r:hex_files\hfuse.hex:i -Ulfuse:r:hex_files\lfuse.hex:i -Uefuse:r:hex_files\efuse.hex:i -Ulock:r:hex_files\lock.hex:i -Uflash:r:hex_files\flash.hex:i
-
-pause

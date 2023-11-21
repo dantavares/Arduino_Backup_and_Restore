0b55c6abaaab14153d55aa79ac7bf92d66f2ad85
Daniel Tavares <dantavares@gmail.com>
2023-11-21 11:11:57 -0300

    Add files via upload

---
+++ Attiny85/write_attiny85.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+SET COM=COM3
+
+..\avrdude -Cattiny85.conf -v -pattiny85 -cstk500v1 -P%COM% -b19200 -D -Uhfuse:w:hex_files\hfuse.hex:i -Ulfuse:w:hex_files\lfuse.hex:i -Uefuse:w:hex_files\efuse.hex:i -Ulock:w:hex_files\lock.hex:i -Uflash:w:hex_files\flash.hex:i
+
+pause

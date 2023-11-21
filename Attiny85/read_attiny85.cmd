399f844270b6f0b65a980a839ccf718d0ea6a8e0
Daniel Tavares <dantavares@gmail.com>
2023-11-21 11:11:34 -0300

    Update and rename read_attiny85.cmd to Attiny85/read_attiny85.cmd

--- Attiny85/read_attiny85.cmd
+++ Attiny85/read_attiny85.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-SET COM=COM3
+SET COM=COM1
 
 ..\avrdude -Cattiny85.conf -v -pattiny85 -cstk500v1 -P%COM% -b19200 -Uhfuse:r:hex_files\hfuse.hex:i -Ulfuse:r:hex_files\lfuse.hex:i -Uefuse:r:hex_files\efuse.hex:i -Ulock:r:hex_files\lock.hex:i -Uflash:r:hex_files\flash.hex:i
 

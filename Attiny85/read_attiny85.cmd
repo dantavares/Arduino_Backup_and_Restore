3801c686fb0ff85cf46e8879e91e268760f3e510
Daniel Tavares <dantavares@gmail.com>
2023-11-21 11:39:38 -0300

    Update read_attiny85.cmd

--- Attiny85/read_attiny85.cmd
+++ Attiny85/read_attiny85.cmd
@@ -1,6 +1,13 @@
 @echo off
 
+-----------------------
+--Set Arduino COM Port
+-----------------------
 SET COM=COM1
+
+-------------------------------------------------------------------
+-- Do not change the line below unless you know what you are doing!
+-------------------------------------------------------------------
 
 ..\avrdude -Cattiny85.conf -v -pattiny85 -cstk500v1 -P%COM% -b19200 -Uhfuse:r:hex_files\hfuse.hex:i -Ulfuse:r:hex_files\lfuse.hex:i -Uefuse:r:hex_files\efuse.hex:i -Ulock:r:hex_files\lock.hex:i -Uflash:r:hex_files\flash.hex:i
 
